a33b8dd85ad5ad36bd77f0c46b85b8e003e9a674
HarshPriyadarshi18 <harshp.ug23.cs@nitp.ac.in>
2026-05-27 08:05:41 +0530

    Initial PrepAI setup

---
+++ Backend/src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+ 
+const app=express();
+app.use(express.json());
+app.use(cors(
+    {
+        origin:"http://localhost:3000",
+        credentials:true
+    }
+));
+app.use(cookieParser());
+
+app.get("/",(req,res)=>{
+    req.status(200).json({message:"Hello World"});
+});
+
+export default app;
