64ee6331f888332f9b62c6e79f076a1449f8080b
HarshPriyadarshi18 <harshp.ug23.cs@nitp.ac.in>
2026-05-29 07:47:36 +0530

    added auth and middleware protected routes

--- Backend/src/routes/authroutes.js
+++ Backend/src/routes/authroutes.js
@@ -1,6 +1,11 @@
 import express from 'express';
-import { login, signup } from '../controllers/authcontroller.js';
+import { login, signup ,getMe,Logout} from '../controllers/authcontroller.js';
+import {protect} from "../middleware/authmiddleware.js";
+
 const router=express.Router();
 router.post("/login",login);
 router.post("/signup",signup);
+router.get("/me",protect,getMe);
+router.get("/logout",Logout);
+
 export default router;
