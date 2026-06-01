5c6b04885999ac439999a372ac57585be2b2bcdc
HarshPriyadarshi18 <harshp.ug23.cs@nitp.ac.in>
2026-06-01 21:00:43 +0530

    Added protected resume upload API, Multer middleware, and resume model

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
+import resumeRoutes from "./routes/resumeRoutes.js";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authroutes.js";
- 
+ import path from "path";
 const app=express();
 app.use(express.json());
 app.use(cors(
@@ -12,10 +13,11 @@ app.use(cors(
     }
 ));
 app.use(cookieParser());
-
+app.use("/api/resumes",resumeRoutes);
 app.get("/",(req,res)=>{
     res.status(200).json({message:"Hello World"});
 });
+app.use("/uploads",express.static(path.join(path.resolve(), "uploads")));
 
 app.use("/api/auth",authRoutes);
 export default app;
