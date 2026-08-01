d84f66cda8d9b2bba84b1b4f182020cbe06f55c5
HarshPriyadarshi18 <harshp.ug23.cs@nitp.ac.in>
2026-08-01 20:17:40 +0530

    feat:updated

--- Backend/src/config/gemini.js
+++ Backend/src/config/gemini.js
@@ -1,6 +1,3 @@
-import { GoogleGenerativeAI} from '@google/generative-ai';
-import dotenv from "dotenv";
-dotenv.config();
-const genAI=new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+import groq from "./groq.js";
 
-export default genAI;
+export default groq;
